ec2e12e271fe2fffc344245eac91545ea5a85ac5
tulane <dpzhuming@szeastroc.com>
2020-10-25 21:12:28 +0800

    week2_2

---
+++ Week_02/HttpClient/HttpUtilsTest.java
@@ -0,0 +1,7 @@
+public class HttpUtilsTest {
+
+    public static void main(String[] args) {
+        final String s = HttpUtils.get("http://localhost:8808/test", null, null);
+        System.out.println(s);
+    }
+}
---
+++ Week_02/HttpClient/README.md
@@ -0,0 +1,4 @@
+HttpClient客户端
+
+1. HttpUtils 为封装了 HttpClient 并池化的工具类
+2. HttpUtilsTest 为调用 HttpUtils 的测试类
